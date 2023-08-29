84b862ac464b409792a65ef3238fc8dda4a3f9de
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-08-28 21:29:11 -0300

    Implemented Class3

--- Clase2Ejercicio1/Clase2Ejercicio1/Program.cs
+++ Clase2Ejercicio1/Clase2Ejercicio1/Program.cs
@@ -108,6 +108,10 @@ namespace Clase2Ejercicio1
             {
                 Console.WriteLine("Puede disparar ya que adivinó el código");
             }
+            else
+            {
+                Console.WriteLine("No puede disparar ya que no adivinó el código");
+            }
         }
 
         public void Ejercicio6()
@@ -175,6 +179,8 @@ namespace Clase2Ejercicio1
                     case 4:
                         Console.WriteLine("Cuatro");
                         break;
+                    default:
+                        break;
                 }
             }else if (num1 == 0)
             {
@@ -191,6 +197,7 @@ namespace Clase2Ejercicio1
                 }
             }
         }
+
         private int VerifyIntegerInput()
         {
             int num;
@@ -201,13 +208,226 @@ namespace Clase2Ejercicio1
             return num;
         }
     }
+
+    public class Clase3
+    {
+        private int VerifyIntegerInput()
+        {
+            int num;
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Ingrese un numero valido: ");
+            }
+            return num;
+        }
+
+        //Pedir un numero del 1 al 7 y decir el dia de la semana correspondiente
+        public void Ejercicio1()
+        {
+            int num1;
+            Console.WriteLine("Ingrese un numero del 1 al 7: ");
+            num1 = VerifyIntegerInput();
+            switch (num1)
+            {
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 3:
+                    Console.WriteLine("Martes");
+                    break;
+                case 4:
+                    Console.WriteLine("Miercoles");
+                    break;
+                case 5:
+                    Console.WriteLine("Jueves");
+                    break;
+                case 6:
+                    Console.WriteLine("Viernes");
+                    break;
+                case 7:
+                    Console.WriteLine("Sabado");
+                    break;
+                default:
+                    Console.WriteLine("Numero invalido");
+                    break;
+            }
+        }
+
+        //Pedir un numero del 1 al 12 y decir el nombre del mes correspondiente
+        public void Ejercicio2() 
+        {
+            int num1;
+            Console.WriteLine("Ingrese un numerodel 1 al 12: ");
+            num1 = VerifyIntegerInput();
+            switch (num1)
+            {
+                case 1:
+                    Console.WriteLine("Enero");
+                    break;
+                case 2:
+                    Console.WriteLine("Febrero");
+                    break;
+                case 3:
+                    Console.WriteLine("Marzo");
+                    break;
+                case 4:
+                    Console.WriteLine("Abril");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayo");
+                    break;
+                case 6:
+                    Console.WriteLine("Junio");
+                    break;
+                case 7:
+                    Console.WriteLine("Julio");
+                    break;
+                case 8:
+                    Console.WriteLine("Agosto");
+                    break;
+                case 9:
+                    Console.WriteLine("Septiembre");
+                    break;
+                case 10:
+                    Console.WriteLine("Octubre");
+                    break;
+                case 11:
+                    Console.WriteLine("Noviembre");
+                    break;
+                case 12:
+                    Console.WriteLine("Diciembre");
+                    break;
+                default:
+                    Console.WriteLine("Numero invalido");
+                    break;
+            }
+        }
+
+        //pedir una letra si es una vocal imprimir dicha letra en pantalla
+        public void Ejercicio3()
+        {
+            string letra;
+            Console.WriteLine("Escriba una letra");
+            letra = Console.ReadLine();
+            letra = letra.ToLower();
+            switch (letra)
+            {
+                case "a":
+                    Console.WriteLine(letra);
+                    break;
+                case "e":
+                    Console.WriteLine(letra);
+                    break;
+                case "i":
+                    Console.WriteLine(letra);
+                    break;
+                case "o":
+                    Console.WriteLine(letra);
+                    break;
+                case "u":
+                    Console.WriteLine(letra);
+                    break;
+                default:
+                    Console.WriteLine("Respuesta no valida");
+                    break;
+            }
+        }
+
+        //
+        public void Ejercicio4()
+        {
+            int num1 = 1;
+            Console.WriteLine("Ingrese un numero: ");
+            num1 = VerifyIntegerInput();
+            while (num1 != 0)
+            {
+                if (num1 % 2 == 0)
+                {
+                    Console.WriteLine("Es par");
+                }
+                else
+                {
+                    Console.WriteLine("Es impar");
+                }
+                Console.WriteLine("Ingrese un numero: ");
+                num1 = VerifyIntegerInput();
+            }
+            Console.WriteLine("A salido del loop");
+        }
+
+        //ingresar 10 numeros y lueego mostrar en pantalla el promedio de los mismos con do while
+        public void Ejercicio5()
+        {
+            int i = 0;
+            int num1;
+            int suma = 0;
+            do
+            {
+                Console.WriteLine("Ingrese el {0} numero: ",i);
+                num1 = VerifyIntegerInput();
+                suma += num1;
+                i++;
+            } while (i < 10);
+            suma = suma / 10;
+            Console.WriteLine("El promedio es: {0} ", suma);
+        }
+
+        //pedir al usuario que ingrese un numero y mostrarlo en pantalla, que termine cuando sea 0
+        public void Ejercicio6()
+        {
+            int num1;
+            do
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                num1 = VerifyIntegerInput();
+                Console.WriteLine("El numero es: {0} ", num1);
+            } while (num1 == 0);
+        }
+
+        //un programa que lea 10 datos ingresados por el usuario y sume aquellos que sean negativos, mostrarlo en pantalla
+        public void Ejercicio7()
+        {
+            int num1 = 0;
+            int resultado = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                num1 = VerifyIntegerInput();
+                if (num1 < 0)
+                {
+                    resultado += num1;
+                }
+            }
+            Console.WriteLine("El resultado es: {0}", resultado);
+        }
+
+        //un programa que calcule el factorial del numero ingresado ejemplo 5 = 5x4x3x2x1
+        public void Ejercicio8()
+        {
+            int num1;
+            int result= 1;
+            Console.WriteLine("Ingrese un numero: ");
+            num1 = VerifyIntegerInput();
+            for (int i = num1; i > 0; i--)
+            {
+                result *= i;
+            }
+            Console.WriteLine("El resultado es: {0}", result);
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Ejercicios ejercicios = new Ejercicios();
-            ejercicios.Ejercicio8();
+           // Ejercicios ejercicios = new Ejercicios();
+            Clase3 clase3 = new Clase3();
+            clase3.Ejercicio8();
             Console.ReadLine();
-        }   
+        }
     }
 }
