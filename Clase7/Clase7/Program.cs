605d63e276b348b6a3a1ea6b012a32b6e5dc59a5
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-09-18 21:41:06 -0300

    Class Recursividad

---
+++ Clase7/Clase7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FuncionTres("aloha"));
+            Console.ReadLine();
+        }
+
+        static int FuncionUno(int n)
+        {
+            if (n == 1)
+                return n;
+            return n + FuncionUno(n - 1);
+        }
+
+        //Funcion para ver si un numero es binario
+        static bool FuncionDos(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return true;
+            }
+            if (n < 0 || n % 10 != 0 && n % 10 != 1)
+            {
+                return false;
+            }
+            return FuncionDos(n / 10);
+        }
+
+        //Funcion para ver si un numero es binario con string
+        static bool FuncionDosString(int num)
+        {
+            string text = num.ToString();
+            if (text.Length == 1)
+            {
+                return text == "0" || text == "1";
+            }
+
+            string firstDigit = text.Substring(0, 1);
+            string otherPart = text.Substring(1);
+            return FuncionDosString(int.Parse(firstDigit)) && FuncionDosString(int.Parse(otherPart));
+        }
+
+        //Con una funcion recursiva si una palabra es un palindromo
+        static bool FuncionTres(string word)
+        {
+            if (word.Length == 1)
+            {
+                return true;
+            }
+            else if (word[0] != word[word.Length - 1])
+            {
+                return false;
+            }
+            string removedWord = word.Substring(1, word.Length - 2);
+            return FuncionTres(removedWord);
+        }
+    }
+}
---
+++ ClaseFunciones/ClaseFunciones/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseFunciones
+{
+    class Program
+    {
+        const string PLAYER =
+@"\(-.-)/";
+        static void Main(string[] args)
+        {
+            /*int num1;
+            int num2;
+            int result;
+            //Ejercicio1
+            Console.WriteLine("Ingrese un numero");
+            num1 = VerifyIntegerInput();
+            CheckPositive(num1);
+            //Ejercicio2
+            Console.WriteLine("Ingrese un numero");
+            num1 = VerifyIntegerInput();
+            Console.WriteLine("Ingrese el segundo numero");
+            num2 = VerifyIntegerInput();
+            Console.WriteLine($"El resultado es: {Multipy(num1,num2)}");
+            //Ejercicio3
+            Console.WriteLine("Ingrese un numero");
+            num1 = VerifyIntegerInput();
+            CompareToTwenty(num1);
+            //Ejercicio4
+            Console.WriteLine("Ingrese un numero");
+            num1 = VerifyIntegerInput();
+            if (CheckDivisibleByFive(num1))
+            {
+                Console.WriteLine("Es divisible por 5");
+            }
+            else
+            {
+                Console.WriteLine("No es divisible por 5");
+            }
+            //Ejercicio5
+            Console.WriteLine("Ingrese un numero");
+            num1 = VerifyIntegerInput();
+            while (num1 != 0)
+            {
+
+            }
+            Console.ReadLine();*/
+            string movementPlayer = "";
+            string previewMovementPlayer = movementPlayer;
+            Console.WriteLine(PLAYER);
+            while (true)
+            {
+                movementPlayer = Movement(movementPlayer);
+                if (movementPlayer != previewMovementPlayer)
+                {
+                    Console.Clear();
+                    previewMovementPlayer = movementPlayer;
+                }
+                Console.WriteLine(movementPlayer + PLAYER);
+            }
+        }
+
+        static string Movement(string movementPlayer)
+        {
+            int maxDimention = 30;
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    if (movementPlayer != "")
+                    {
+                        movementPlayer = movementPlayer.Remove(0,1);
+                    }
+                    break;
+                case ConsoleKey.UpArrow:
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (movementPlayer.Length < maxDimention)
+                    {
+                        movementPlayer += " ";
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    break;
+                default:
+                    break;
+            }
+            return movementPlayer;
+        }
+
+        static int VerifyIntegerInput()
+        {
+            int num;
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Ingrese un numero valido: ");
+            }
+            return num;
+        }
+
+        static void CheckPositive(int num)
+        {
+            if (num >= 0)
+            {
+                Console.WriteLine("El numero es positivo");
+            }
+            else
+            {
+                Console.WriteLine("El numero es negativo");
+            }
+        }
+
+        static int Multipy(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        static void CompareToTwenty(int num)
+        {
+            if (num < 20)
+            {
+                Console.WriteLine("Es menor que 20");
+            }
+            else if (num > 20)
+            {
+                Console.WriteLine("Es mayor que 20");
+            }
+            else
+            {
+                Console.WriteLine("Es igual a 20");
+            }
+        }
+
+        static bool CheckDivisibleByFive(int num)
+        {
+            return num % 5 == 0;
+        }
+
+        static void CheckOdd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Es un numero par");
+            }
+            else
+            {
+                Console.WriteLine("Es un numero impar");
+            }
+        }
+    }
+}
